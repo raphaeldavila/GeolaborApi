884708fb179b608c9b9f348223a03045017fddcd
Raphael D Avila <raphael.ribas@brpolen.com.br>
2022-11-06 09:13:10 -0300

    create api geolabor

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import { router } from "./routes"
+import cors from "cors"
+import mongoose from "mongoose"
+import 'dotenv/config'
+
+const app = express();
+const db: string = (process.env.DATABASE_URL as string);
+const port: string = (process.env.PORT as string);
+mongoose.connect(db);
+
+app.use(cors())
+app.use(express.json());
+app.use(router);
+
+app.listen(port, () => {
+    console.log("Server is listening: " + port);
+});
